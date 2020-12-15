fa2481620fb0fd931132841bd32b83cfa77b5fcb
Risqi Ahmad Sultoni <ahmadreys@gmail.com>
2020-12-15 17:41:44 +0700

    Create BI_DEV_Javascript.js

---
+++ BI_DEV_Javascript.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  element.parentNode.removeChild(element);
+}
+
+// Example case. 
+document.body.innerHTML = `
+<div class="image">
+  <img src="https://goo.gl/kjzfbE" alt="First">
+  <button class="remove">X</button>
+</div>
+<div class="image">
+  <img src="https://goo.gl/d2JncW" alt="Second">
+  <button class="remove">X</button>
+</div>`;
+
+setup();
+
+document.getElementsByClassName("remove")[0].click();
+console.log(document.body.innerHTML);
